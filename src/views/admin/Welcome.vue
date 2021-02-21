981d6b5d05050006853c80a494458711409cf420
方嘉乐 <8109863+fang-jiale@user.noreply.gitee.com>
2021-02-21 21:16:40 +0800

    "后台管理的欢迎页面"

---
+++ src/views/admin/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" align="center">
+
+    <h1>欢迎来到博客后台管理中心，请开始你的旅程!</h1>
+    <el-container>
+      <div class="main-pic" align="middle" style="margin:auto">
+        <img src="http://m.qpic.cn/psc?/V53KcXfb1umonn4HbITu3rINxs43TczD/45NBuzDIW489QBoVep5mcdt1OksodJaq5.VcMVCBrgYcYWXybznv8Nh0CHqO1LRbuNlR4lSIjyubrDss9ZS3UTOOpbBkOAQucKvMdx8*Dts!/b&bo=vAIBA7wCAQMCOR0!&rf=viewer_4" class="main-pic" width="400" height="400" align="middle">
+      </div>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome'
+}
+</script>
+
+<style scoped>
+
+</style>
